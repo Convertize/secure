c94380082b3005c48f1630bd38c1e2a510dddf9b
Gabriel Galiaso <gabriel@convertize.com.br>
2022-02-18 11:57:36 -0300

    Seals script

--- src/static/js/seal.js
+++ src/static/js/seal.js
@@ -3,12 +3,24 @@ document.currentScript = document.currentScript || (function() {
   return scripts[scripts.length - 1];
 })();
 (function() {
+	const default_size = "162x196";
+	let size = default_size;
 	const schama_name = document.currentScript.getAttribute('schama_name');
 	const content_id = document.currentScript.getAttribute('content');
+	try { size = document.currentScript.getAttribute('size'); }catch (e) {}
+	
+	if (size == undefined || size == null) size = default_size;
+
+
 	const content = document.getElementById(content_id);
+
+	const [width, height] = size.split("x");
 	
 	let shield_image = document.createElement("img");
 	shield_image.src = "https://secure.convertize.com.br/static/media/pci-shield.png";
+
+	if (width != undefined) shield_image.width = width;
+	if (height != undefined) shield_image.height = height;
 
 	let valid_url = document.createElement("a");
 	valid_url.href = "https://secure.convertize.com.br/?s=" + schama_name;
